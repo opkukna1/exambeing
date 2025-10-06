651a7ca61df4a5e21000778707fc41e79e374daa
opkukna1 <Opsiddh41@gmail.com>
2025-10-06 23:41:36 +0530

    Add files via upload

---
+++ android/app/src/main/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.history_metallum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // This class is intentionally left empty.
+}
