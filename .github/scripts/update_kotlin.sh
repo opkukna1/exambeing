5b1a1b4132f90cf03520b61587fe75ffe11db1ae
opkukna1 <Opsiddh41@gmail.com>
2025-10-07 00:53:00 +0530

    Update update_kotlin.sh

--- .github/scripts/update_kotlin.sh
+++ .github/scripts/update_kotlin.sh
@@ -15,7 +15,7 @@ if grep -q "ext.kotlin_version" "$BUILD_FILE"; then
     sed -i "s/ext.kotlin_version = .*/ext.kotlin_version = '1.9.22'/" "$BUILD_FILE"
     echo "Kotlin version updated to 1.9.22"
 else
-    # A more robust way to insert the line at the top of the file
-    awk 'NR==1{print "ext.kotlin_version = \'1.9.22\'"}1' "$BUILD_FILE" > tmpfile && mv tmpfile "$BUILD_FILE"
+    # This is a simpler and more reliable way to insert the line at the top.
+    sed -i "1i ext.kotlin_version = '1.9.22'" "$BUILD_FILE"
     echo "Added Kotlin version 1.9.22"
 fi
