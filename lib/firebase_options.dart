fa773f2f6e391e09962a9a50c1c4db863db7175c
opkukna1 <Opsiddh41@gmail.com>
2025-10-07 19:55:17 +0530

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => const FirebaseOptions(
+    apiKey: "YOUR_API_KEY",
+    appId: "YOUR_APP_ID",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  );
+}
