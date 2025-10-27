ca48d4c2d450c74078654b46367a6a6c15b4b167
opkukna1 <Opsiddh41@gmail.com>
2025-10-28 01:56:29 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build.gradle.kts
 
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
