5ff36db46de715645baff429ac446cab6f800673
opkukna1 <Opsiddh41@gmail.com>
2025-10-28 02:05:14 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,28 @@
-// Top-level build.gradle.kts
-
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("com.google.gms.google-services") apply false
 }
 
-task<Delete>("clean") {
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Firebase tools (अगर Kotlin script में चाहिए)
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
