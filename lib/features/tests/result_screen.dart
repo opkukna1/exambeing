00178b4472283f0d298c2b8cc242a2bc22a873ea
opkukna1 <Opsiddh41@gmail.com>
2025-11-10 15:52:31 +0530

    Update result_screen.dart

--- lib/features/tests/result_screen.dart
+++ lib/features/tests/result_screen.dart
@@ -1,5 +1,5 @@
 import 'dart.typed_data';
-import 'package.flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
