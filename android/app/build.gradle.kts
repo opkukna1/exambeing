7d41598d85dacaa707a3d2a6881df6185b905358
opkukna1 <Opsiddh41@gmail.com>
2025-12-13 18:00:27 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.opkukna.exambeing"
         minSdk = 23
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
     }
 
     buildTypes {
